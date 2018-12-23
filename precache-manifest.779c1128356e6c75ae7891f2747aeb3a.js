7e3e8a60a72ed44950d567bdf54e2f3acf15d4a5
Tobias Kuhn <xcq1.tk@gmail.com>
2018-12-23 19:29:41 +0100

    Updates

---
+++ precache-manifest.779c1128356e6c75ae7891f2747aeb3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc9a92b8b362b59c64bb",
+    "url": "/memory-numbers/static/js/runtime~main.fc9a92b8.js"
+  },
+  {
+    "revision": "53abf6155bd67b6d98ef",
+    "url": "/memory-numbers/static/js/main.53abf615.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/memory-numbers/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "53abf6155bd67b6d98ef",
+    "url": "/memory-numbers/static/css/main.e8ed1964.chunk.css"
+  },
+  {
+    "revision": "dcaec906e1b8c59e0a215f4a1ec6ebd6",
+    "url": "/memory-numbers/index.html"
+  }
+];
